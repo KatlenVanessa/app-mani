688d5bd45574dd7af859fdae2246d4c8bea6c3fc
Katlen Vanessa <94211156+KatlenVanessa@users.noreply.github.com>
2023-06-16 18:25:40 -0300

    up

--- admin/src/components/Home.jsx
+++ admin/src/components/Home.jsx
@@ -1,9 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { getPosts } from "../api/post";
 
-export default function Home(){
-    return(
-            <div>
-                <h1>Home</h1>
-            </div>
-        )
+let pageNo = 0
+const POST_LIMIT = 9
+
+export default function Home() {
+    const [posts, setPosts] = useState([])
+    const fetchPosts = async () => {
+        const { error, posts } = await getPosts(pageNo, POST_LIMIT);
+
+        if (error) {
+            return console.log(error);
+
+        }
+
+        setPosts(posts);
+    };
+
+    useEffect(() => {
+        fetchPosts()
+    }, []);
+    return <div>{
+        posts.map(post => {
+            return <div>{post.title}</div>
+        })
+    }</div>;
 }
--- admin/src/components/NavBar.jsx
+++ admin/src/components/NavBar.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import { AiFillFileAdd, AiOutlineHome } from 'react-icons/ai';
 import { NavLink } from 'react-router-dom';
 
-const NavItem = ({to, value, closed, Icon}) => {
+const NavItem = ({ to, value, closed, Icon }) => {
     const commonClasses = 'flex items-center space-x-2 w-full p-2 block whitespace-nowrap';
     const activeClass = commonClasses + ' bg-blue-500 text-white';
     const inActiveClass = commonClasses + 'text-gray-500';
 
-    return(
-        <NavLink 
-            className={({isActive}) => (isActive ? activeClass : inActiveClass)} to={to}
+    return (
+        <NavLink
+            className={({ isActive }) => (isActive ? activeClass : inActiveClass)} to={to}
         >
             {Icon}
-            <span 
+            <span
                 className={
-                    closed 
+                    closed
                         ? 'w-0 transition-width overflow-hidden'
                         : 'w-ful transition-width overflow-hidden'
-                } 
+                }
             >
                 {value}
             </span>
@@ -25,26 +25,23 @@ const NavItem = ({to, value, closed, Icon}) => {
     );
 };
 
-const NavBar = ({closed}) => {
+const NavBar = ({ closed }) => {
     return (
         <nav>
-            <div>
-                <img></img>
-            </div>
             <ul>
                 <li>
-                   <NavItem closed={closed} to='/' value='Home' Icon={<AiOutlineHome size={24} />} />
+                    <NavItem closed={closed} to='/' value='Home' Icon={<AiOutlineHome size={24} />} />
                 </li>
                 <li>
                     <NavItem
-                    closed={closed}
-                    to='/create-post'
-                    value='Create Post'
-                    Icon={<AiFillFileAdd size={24} />} 
+                        closed={closed}
+                        to='/create-post'
+                        value='Create Post'
+                        Icon={<AiFillFileAdd size={24} />}
                     />
                 </li>
             </ul>
-            
+
         </nav>
     );
 }
