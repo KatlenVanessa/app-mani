48a9d4a9da0de64d865af27970a049347f480754
Katlen Vanessa <94211156+KatlenVanessa@users.noreply.github.com>
2023-06-16 18:28:06 -0300

    up

--- backend/app.js
+++ backend/app.js
@@ -4,15 +4,20 @@ const express = require("express");
 require("dotenv").config();
 const morgan = require('morgan');
 const postRouter = require('./routers/post');
+const cors = require('cors');
 
 const app = express();
+app.use(cors({
+    origin: 'https://localhost:3000'
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use('/api/post', postRouter);
 app.use((err, req, res, next) => {
-    res.status(500).json({error: err.message});
+    res.status(500).json({ error: err.message });
 });
 
 const PORT = process.env.PORT;
-app.listen(PORT, () => {console.log("port is listening on " + PORT);
+app.listen(PORT, () => {
+    console.log("port is listening on " + PORT);
 });
